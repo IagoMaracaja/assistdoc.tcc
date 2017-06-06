2a52a6bc2836ded14ce5488c9263f10a8219c2b7
Iago Maracaja <iago079@gmail.com>
2017-06-06 16:15:08 -0300

    adjust of UI

--- app/src/main/java/com/tcc/iago/assistdoctor/views/LoginActivity.java
+++ app/src/main/java/com/tcc/iago/assistdoctor/views/LoginActivity.java
@@ -1,12 +1,17 @@
 package com.tcc.iago.assistdoctor.views;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.text.InputFilter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import com.tcc.iago.assistdoctor.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -15,6 +20,11 @@ import butterknife.OnClick;
  */
 public class LoginActivity extends AppCompatActivity {
 
+    public static final String TOKEN = "token";
+    private static final int MAX_LENGTH_EDITTEXT = 6;
+    @BindView(R.id.et_token_main)
+    protected TextView mEdToken;
+    private AlertDialog mAlertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +33,82 @@ public class LoginActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        configElementsOfView();
+    }
 
     @OnClick(R.id.login_btn_enjoy)
     public void callScreenShared(){
         Intent viewScreenShared = new Intent(LoginActivity.this, ViewScreenSharedActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString(TOKEN, mEdToken.getText().toString());
+        viewScreenShared.putExtras(bundle);
         startActivity(viewScreenShared);
     }
+
+    public void configElementsOfView() {
+        mEdToken.setFilters(new InputFilter[]{
+                new InputFilter.AllCaps(),
+                new InputFilter.LengthFilter(MAX_LENGTH_EDITTEXT)
+        });
+    }
+    @OnClick(R.id.tv_token_help)
+    public void onClickHelp(){
+        showTokenHelper();
+    }
+
+
+    /**
+     * Open a dialog for help information about Access Token.
+     */
+    private void CallDialogForHelp() {
+        LayoutInflater li = getLayoutInflater();
+
+        View view = li.inflate(R.layout.alert_dialog_help, null);
+        view.findViewById(R.id.bt).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                mAlertDialog.dismiss();
+            }
+        });
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(view);
+        mAlertDialog = builder.create();
+        mAlertDialog.setCancelable(false);
+        mAlertDialog.show();
+    }
+
+
+    /**
+     * Open a dialog to show a error about any validation.
+     */
+    private void showTokenHelper() {
+        LayoutInflater li = getLayoutInflater();
+
+        if (mAlertDialog == null) {
+
+            View view = li.inflate(R.layout.alert_dialog_help, null);
+            view.findViewById(R.id.bt).setOnClickListener(new View.OnClickListener() {
+                public void onClick(View arg0) {
+
+                        mAlertDialog.dismiss();
+                        mAlertDialog = null;
+
+                }
+            });
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setView(view);
+            if (!isFinishing()) {
+                mAlertDialog = builder.create();
+                mAlertDialog.setCancelable(false);
+                mAlertDialog.show();
+            }
+
+        }
+    }
+
+
 }
--- app/src/main/java/com/tcc/iago/assistdoctor/views/ViewScreenSharedActivity.java
+++ app/src/main/java/com/tcc/iago/assistdoctor/views/ViewScreenSharedActivity.java
@@ -1,11 +1,21 @@
 package com.tcc.iago.assistdoctor.views;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.tcc.iago.assistdoctor.R;
 
@@ -18,10 +28,14 @@ import butterknife.ButterKnife;
 
 public class ViewScreenSharedActivity extends Activity {
 
+    private static final String URL = "http://doctor-assistance.herokuapp.com/view?hash=";
+
     @BindView(R.id.vss_webview)
     protected WebView mWebView;
+    @BindView(R.id.tv_webview_status)
+    protected TextView mTvWebviewStatus;
 
-    private static final String URL = "https://assist-doctor.herokuapp.com/demos/screen-sharing.html#28aoyq6kesx";
+    private String mHash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,24 +43,99 @@ public class ViewScreenSharedActivity extends Activity {
         setContentView(R.layout.activity_view_screen_shared);
         ButterKnife.bind(this);
 
-        openWebVieW();
     }
 
-    public void openWebVieW(){
+    @Override
+    protected void onResume() {
+        mHash = getIntent().getExtras().getString(LoginActivity.TOKEN);
+
+        newOpenWebVieW(mHash);
+        super.onResume();
+    }
+
+    public void openWebVieW(String hash) {
 
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.getSettings().setBuiltInZoomControls(false);
-
-         mWebView.loadUrl(URL);
+        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
 
 
         mWebView.setWebChromeClient(new WebChromeClient() {
+
             public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
                 Toast.makeText(ViewScreenSharedActivity.this, "ErrorCode = " + errorCode, Toast.LENGTH_SHORT).show();
 
             }
         });
 
+        mWebView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                mTvWebviewStatus.setText("Page Started");
+
+                Log.d("WebView", "onPageStarted " + url);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                mTvWebviewStatus.setText("Page Finished");
+                Log.d("WebView", "onPageFinished " + url);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                mTvWebviewStatus.setText("Error: "+error.toString());
+                super.onReceivedError(view, request, error);
+            }
+        });
+
+        mWebView.loadUrl(URL + hash);
+    }
+
+    WebChromeClient webChromeClient = new WebChromeClient();
+    WebViewClient webViewClient = new WebViewClient();
+
+    public void newOpenWebVieW(String hash){
+
+        mWebView.setWebChromeClient(webChromeClient);
+        mWebView.setWebViewClient(new MyWebViewClient());
+        mWebView.getSettings().setJavaScriptEnabled(true);
+
+        mWebView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        mWebView.getSettings().setLoadWithOverviewMode(true);
+        mWebView.getSettings().setUseWideViewPort(true);
+
+        mWebView.loadUrl(URL + hash);
+
+    }
+
+    public class MyWebChromeClient extends WebChromeClient {
+
+        @Override
+        public void onShowCustomView(View view, CustomViewCallback callback) {
+            // TODO Auto-generated method stub
+            super.onShowCustomView(view, callback);
+            if (view instanceof FrameLayout) {
+                FrameLayout frame = (FrameLayout) view;
+                if (frame.getFocusedChild() instanceof VideoView) {
+                    VideoView video = (VideoView) frame.getFocusedChild();
+                    frame.removeView(video);
+                    video.start();
+                }
+            }
+
+        }
+    }
+
+    public class MyWebViewClient extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 
 }
+
