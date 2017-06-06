281ea99058acbac662ba27cebccecb5146f50bb4
Iago Maracaja <iago079@gmail.com>
2017-06-06 16:32:38 -0300

    adjust of UI

---
+++ app/src/main/java/com/tcc/iago/assistdoctor/util/Constants.java
@@ -0,0 +1,13 @@
+package com.tcc.iago.assistdoctor.util;
+
+/**
+ * Created by iago on 06/06/17.
+ */
+
+public class Constants {
+
+
+    public static final int PASSWORD_MAX_LENGTH = 6;
+
+    public static final String CODE_PASSWORD_REGEX = "[A-Za-z0-9]*";
+}
--- app/src/main/java/com/tcc/iago/assistdoctor/views/LoginActivity.java
+++ app/src/main/java/com/tcc/iago/assistdoctor/views/LoginActivity.java
@@ -1,15 +1,19 @@
 package com.tcc.iago.assistdoctor.views;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.InputFilter;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.tcc.iago.assistdoctor.R;
+import com.tcc.iago.assistdoctor.util.Constants;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -36,16 +40,20 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        configElementsOfView();
+        this.configElementsOfView();
     }
 
     @OnClick(R.id.login_btn_enjoy)
-    public void callScreenShared(){
-        Intent viewScreenShared = new Intent(LoginActivity.this, ViewScreenSharedActivity.class);
-        Bundle bundle = new Bundle();
-        bundle.putString(TOKEN, mEdToken.getText().toString());
-        viewScreenShared.putExtras(bundle);
-        startActivity(viewScreenShared);
+    public void callScreenShared() {
+        if (isValidToken(mEdToken.getText().toString())) {
+            Intent viewScreenShared = new Intent(LoginActivity.this, ViewScreenSharedActivity.class);
+            Bundle bundle = new Bundle();
+            bundle.putString(TOKEN, mEdToken.getText().toString());
+            viewScreenShared.putExtras(bundle);
+            startActivity(viewScreenShared);
+        } else {
+            showMessageError(true, getString(R.string.invalid_token));
+        }
     }
 
     public void configElementsOfView() {
@@ -53,33 +61,13 @@ public class LoginActivity extends AppCompatActivity {
                 new InputFilter.AllCaps(),
                 new InputFilter.LengthFilter(MAX_LENGTH_EDITTEXT)
         });
+        this.addTokenImeOption();
     }
+
     @OnClick(R.id.tv_token_help)
-    public void onClickHelp(){
+    public void onClickHelp() {
         showTokenHelper();
     }
-
-
-    /**
-     * Open a dialog for help information about Access Token.
-     */
-    private void CallDialogForHelp() {
-        LayoutInflater li = getLayoutInflater();
-
-        View view = li.inflate(R.layout.alert_dialog_help, null);
-        view.findViewById(R.id.bt).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0) {
-                mAlertDialog.dismiss();
-            }
-        });
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setView(view);
-        mAlertDialog = builder.create();
-        mAlertDialog.setCancelable(false);
-        mAlertDialog.show();
-    }
-
 
     /**
      * Open a dialog to show a error about any validation.
@@ -93,8 +81,8 @@ public class LoginActivity extends AppCompatActivity {
             view.findViewById(R.id.bt).setOnClickListener(new View.OnClickListener() {
                 public void onClick(View arg0) {
 
-                        mAlertDialog.dismiss();
-                        mAlertDialog = null;
+                    mAlertDialog.dismiss();
+                    mAlertDialog = null;
 
                 }
             });
@@ -110,5 +98,54 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
+    private void addTokenImeOption() {
+        mEdToken.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_GO) {
+                    callScreenShared();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    /**
+     * Check if the password has 6 characters (Letters and Numbers)
+     *
+     * @param token
+     * @return
+     */
+    public boolean isValidToken(final String token) {
+        String cleanPassword = token.trim();
+        boolean returnValue;
+        if (cleanPassword.length() < Constants.PASSWORD_MAX_LENGTH || cleanPassword.length() > Constants.PASSWORD_MAX_LENGTH) {
+            returnValue = false;
+        } else if (!cleanPassword.matches(Constants.CODE_PASSWORD_REGEX)) {
+            returnValue = false;
+        } else {
+            returnValue = true;
+        }
+        return returnValue;
+    }
+
+    /**
+     * Show message error
+     *
+     * @param showMessageError
+     * @param messageError
+     */
+    public void showMessageError(boolean showMessageError, String messageError) {
+        LinearLayout errorContainer = (LinearLayout) findViewById(R.id.ll_error_container);
+        TextView errorMessage = (TextView) findViewById(R.id.tv_message_error);
+
+        if (showMessageError) {
+            errorContainer.setVisibility(View.VISIBLE);
+            errorMessage.setText(messageError);
+        } else {
+            errorContainer.setVisibility(View.GONE);
+        }
+    }
 
 }
